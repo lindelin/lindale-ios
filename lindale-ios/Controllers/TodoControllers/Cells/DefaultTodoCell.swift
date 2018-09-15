2b7ec6c07a9e462b2005e2440807697faf5a7d6e
lindelea <kgo@lindelin.org>
2018-09-15 20:46:28 +0900

    Todo Cell

---
+++ lindale-ios/Controllers/TodoControllers/Cells/DefaultTodoCell.swift
@@ -0,0 +1,24 @@
+//
+//  DefaultTodoCell.swift
+//  lindale-ios
+//
+//  Created by Jie Wu on 2018/09/15.
+//  Copyright © 2018年 lindelin. All rights reserved.
+//
+
+import UIKit
+
+class DefaultTodoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
