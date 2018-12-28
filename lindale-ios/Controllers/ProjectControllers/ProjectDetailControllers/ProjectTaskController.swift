1cb9385d3dd57c897b98052709e5b0241a036387
lindelea <kgo@lindelin.org>
2018-12-28 12:34:12 +0900

    Floating Button

--- lindale-ios/Controllers/ProjectControllers/ProjectDetailControllers/ProjectTaskController.swift
+++ lindale-ios/Controllers/ProjectControllers/ProjectDetailControllers/ProjectTaskController.swift
@@ -8,10 +8,37 @@
 
 import UIKit
 import KRProgressHUD
+import CircleMenu
 
 class ProjectTaskController: UITableViewController {
     
     static let identity = "ProjectTasks"
+    
+    var addBtn = UIButton(type: .custom)
+    
+    func floatingButton() {
+        
+        guard let window = UIApplication.shared.keyWindow else {
+            return
+        }
+        
+        addBtn.frame = CGRect(x: window.frame.width - 64 - 20 ,
+                              y: window.frame.height - Size.tabBarHeight - 64 - 20,
+                              width: 64,
+                              height: 64)
+        //btn.setTitle("+", for: .normal)
+        addBtn.backgroundColor = Colors.themeGreen
+        addBtn.setImage(UIImage(named: "plus-56"), for: .normal)
+        addBtn.tintColor = UIColor.white
+        addBtn.clipsToBounds = true
+        addBtn.layer.cornerRadius = addBtn.frame.size.width / 2.0
+        addBtn.layer.shadowRadius = 10
+        addBtn.layer.shadowOpacity = 0.3
+        addBtn.layer.shadowColor = Colors.themeBaseSub.cgColor
+        addBtn.layer.shadowOffset = CGSize(width: 2, height: 2)
+        
+        window.addSubview(addBtn)
+    }
     
     var parentNavigationController: UINavigationController?
     var project: ProjectCollection.Project!
@@ -25,6 +52,29 @@ class ProjectTaskController: UITableViewController {
         refreshControl?.addTarget(self, action: #selector(self.loadData), for: .valueChanged)
         
         self.loadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        floatingButton()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        addBtn.removeFromSuperview()
+    }
+    
+    override func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
+        UIView.transition(with: addBtn, duration: 0.5, options: .transitionCrossDissolve, animations: {
+            self.addBtn.isHidden = true
+        })
+        
+    }
+    
+    override func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        UIView.transition(with: addBtn, duration: 0.5, options: .transitionCrossDissolve, animations: {
+            self.addBtn.isHidden = false
+        })
     }
     
     @objc func loadData() {
