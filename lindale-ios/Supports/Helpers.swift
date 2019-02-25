aa6c5524c0b6547d41e159f2eafe10fbe9707051
lindelea <kgo@lindelin.org>
2019-02-25 17:01:48 +0900

    Fix bug

--- lindale-ios/Supports/Helpers.swift
+++ lindale-ios/Supports/Helpers.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import KRProgressHUD
 
 extension UIViewController {
     func showAlert(title: String?, message: String?) {
@@ -22,13 +23,15 @@ extension UIViewController {
     }
     
     func logout() {
-        if (OAuth.logout()) {
-            let storyboard = UIStoryboard(name:"Login", bundle: nil)
-            let loginController = storyboard.instantiateViewController(withIdentifier: "Login")
-            UIView.transition(from: self.tabBarController!.view, to: loginController.view, duration: 0.6, options: [.transitionCrossDissolve], completion: {
-                _ in
-                UIApplication.shared.keyWindow?.rootViewController = loginController
-            })
+        NetworkProvider.main.message(request: .logout) { (_) in
+            if (OAuth.logout()) {
+                let storyboard = UIStoryboard(name:"Login", bundle: nil)
+                let loginController = storyboard.instantiateViewController(withIdentifier: "Login")
+                UIView.transition(from: self.tabBarController!.view, to: loginController.view, duration: 0.6, options: [.transitionCrossDissolve], completion: {
+                    _ in
+                    UIApplication.shared.keyWindow?.rootViewController = loginController
+                })
+            }
         }
     }
     
@@ -45,27 +48,6 @@ extension UIViewController {
             errorAlert.addAction(okAction)
             
             self.present(errorAlert, animated: true, completion: nil)
-            // TODO: 刷新令牌
-//            OAuth.refresh { (oauth) in
-//                if let _ = oauth {
-//                    self.showAlert(title: nil, message: "Network Error")
-//                } else {
-//                    let errorAlert = UIAlertController(title: "認証エラー", message: "ログインしてください。", preferredStyle: .alert)
-//
-//                    let okAction = UIAlertAction(title: "OK", style: .default, handler: { (action: UIAlertAction) in
-//                        errorAlert.dismiss(animated: true, completion: nil)
-//                        self.logout()
-//                    })
-//
-//                    errorAlert.addAction(okAction)
-//
-//                    if self.presentingViewController == nil {
-//                        self.view.window?.rootViewController?.present(errorAlert, animated: true, completion: nil)
-//                    }else {
-//                        self.present(errorAlert, animated: true, completion: nil)
-//                    }
-//                }
-//            }
         } else if UserDefaults.dataSuite.bool(forOAuthKey: .hasAuthorizationError) {
             UserDefaults.dataSuite.set(false, forOAuthKey: .hasAuthorizationError)
             let errorAlert = UIAlertController(title: "権限エラー", message: "権限がありません。", preferredStyle: .alert)
